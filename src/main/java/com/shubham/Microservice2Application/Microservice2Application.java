a14bc515220da130aff4a05ca07fac3ee7fe2f83
Shubham Singh <singh.shbhm90@gmail.com>
2021-12-13 23:14:35 +0530

    add port check for multiple instance

--- src/main/java/com/shubham/Microservice2Application/Microservice2Application.java
+++ src/main/java/com/shubham/Microservice2Application/Microservice2Application.java
@@ -1,7 +1,9 @@
 package com.shubham.Microservice2Application;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +20,21 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableDiscoveryClient
 public class Microservice2Application {
 
+	@Autowired
+	private ServerProperties serverProperties;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Microservice2Application.class, args);
 	}
 
 	@GetMapping
 	public ResponseEntity helloMessage(){
+		/**
+		 * Some identifier for the running instance
+		 *
+		 * The running portNumber
+		 */
+		System.out.println("The port number hit is :" + serverProperties.getPort());
 		return new ResponseEntity("Hello from the Microservice 2", HttpStatus.OK);
 	}
 }
